d641440cc914e2bb092758f143e847c8f1128084
Brendan Rafeld <bjrafeld@gmail.com>
2013-02-22 17:29:45 -0800

    Added Point Class and most of Vector class functionality. Added .cpp file with code for header.

---
+++ SupportClasses.cpp
@@ -0,0 +1,82 @@
+#include <SupportClasses.h>
+
+Vector::Vector() {
+	this->x = 0.0;
+	this->y = 0.0;
+	this->z = 0.0;
+}
+
+Vector::Vector(float x, float y, float z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+Vector::Vector operator+(Vector v) {
+	Vector result;
+	result.x = this->x + v.x;
+	result.y = this->y + v.y;
+	result.z = this->z + v.z;
+	return result;
+}
+
+Vector::Vector operator-(Vector v) {
+	Vector result;
+	result.x = this->x - v.x;
+	result.y = this->y - v.y;
+	result.z = this->z - v.z;
+	return result;
+}
+
+Vector::Vector operator*(float scalar) {
+	Vector result;
+	result.x = this.x*scalar;
+	result.y = this.y*scalar;
+	result.z = this.z*scalar;
+	return result;
+}
+
+Vector::Vector operator/(float scalar) {
+	Vector result;
+	result.x = this.x/scalar;
+	result.y = this.y/scalar;
+	result.z = this.z/scalar;
+	return result;
+}
+
+Vector::Vector normalize() {
+	Vector result;
+	float magnitude = sqrt(sqr(this->x)+sqr(this->y)+sqr(this->z));
+	result.x = this->x/magnitude;
+	result.y = this->y/magnitude;
+	result.z = this->z/magnitude;
+	return result;
+}
+
+Point::Point() {
+	this->x = 0.0;
+	this->y = 0.0;
+	this->z = 0.0;
+}
+
+Point::Point(float x, float y, float z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+Point::Point operator+(Point p) {
+	Point result;
+	result.x = this->x + p.x;
+	result.y = this->y + p.y;
+	result.z = this->z + p.z;
+	return result;
+}
+
+Point::Point operator-(Point p) {
+	Point result;
+	result.x = this->x - p.x;
+	result.y = this->y - p.y;
+	result.z = this->z - p.z;
+	return result;
+}
--- SupportClasses.h
+++ SupportClasses.h
@@ -1,15 +1,28 @@
 //Vector Class
-
 class Vector;
 
 class Vector {
     float x, y, z;
     
 public:
+	Vector();
     Vector(float x, float y, float z);
     Vector operator+(Vector v);
     Vector operator-(Vector v);
-    Vector operator*(Vector v);
-    Vector operator/(Vector v)
+    Vector operator*(float scalar);
+    Vector operator/(float scalar)
     Vector noramlize();
 };
+
+class Point;
+
+//Point Class
+class Point {
+	float x, y, z;
+
+public:
+	Point();
+	Point(float x, float y, float z);
+	Point operator+(Point p);
+	Point operator-(Point p);
+}
