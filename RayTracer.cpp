782dbc84598d1376fed72f19c9480ace735cf55d
Gloudas <gloudas@ubuntu.(none)>
2013-02-27 20:14:00 -0800

    added Sampler class

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,8 +1,23 @@
 #include "RayTracer.h"
+//#include "SupportClasses.h"
 
 void RayTracer::trace(Ray & ray, int depth, Color* color) {
 	if(depth >1) {
 		color->setColor(0.0, 0.0, 0.0);
 		return;
 	}
+}
+
+Sampler::Sampler(int screenWidth, int screenHeight) {
+	this->screenWidth = screenWidth;
+	this->screenHeight = screenHeight;
+	xPixel = yPixel = 0;
+}
+
+bool getSample(Sample* sample) {
+	// update this method in the future to allow for multiple sampling / anti-aliasing 
+	sample->x = this->xPixel;
+	sample->y = this->yPixel;
+	this->xPixel += 1;
+	this->yPixel += 1;
 }
--- RayTracer.h
+++ RayTracer.h
@@ -1,9 +1,16 @@
 #include "SupportClasses.h"
 
 class RayTracer;
+class Sampler;
 
-RayTracer {
-
+class RayTracer {
 public:
 	void trace(Ray & ray, int depth, Color* color);
+};
+
+class Sampler {
+public:
+	int xPixel, yPixel, screenWidth, screenHeight;
+	Sampler(int screenWidth, int screenHeight);
+	bool getSample(Sample* sample);
 };
