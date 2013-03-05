9c868657a30d0ae0f9bba32464cb0ea0cdf21384
Gloudas <gloudas@ubuntu.(none)>
2013-03-04 20:50:56 -0800

    added shading files

---
+++ Shading.h
@@ -0,0 +1,15 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include "SupportClasses.h"
+
+using namespace std;
+
+class Shader;
+
+class Shader {
+public:
+	Shader();
+	Color shading(LocalGeo local, BRDF brdf, Ray* lray, Color* lcolor, const Point camera);
+	Vector3 reflectedVector(Vector3 lightDirection, Vector3 normal);
+};
